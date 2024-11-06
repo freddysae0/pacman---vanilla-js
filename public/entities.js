59dae353ea177ee0fb7665aa60f7b2bb321172ce
freddysae0 <freddysae00@gmail.com>
2024-11-06 12:27:19 +0100

    Logic for the game

--- public/entities.js
+++ public/entities.js
@@ -1,4 +1,6 @@
+
 const scale = 25;
+let renderCounter = 0;
 export const Direction = {
   LEFT: 1,
   RIGHT: 2,
@@ -6,18 +8,122 @@ export const Direction = {
   DOWN: 4,
   NO_MOVE: 0,
 };
+export const enemy_route = Array.from({ length: 31 }, () => new Array(31).fill(0));
+enemy_route[2][2] = Direction.RIGHT;
+enemy_route[2][7] = Direction.DOWN;
+enemy_route[6][7] = Direction.LEFT;
+enemy_route[6][2] = Direction.UP;
+
+
+enemy_route[2][22] = Direction.RIGHT;
+enemy_route[2][27] = Direction.DOWN;
+enemy_route[6][27] = Direction.LEFT;
+enemy_route[6][22] = Direction.UP;
+
+enemy_route[27][7] = Direction.UP;
+enemy_route[24][7] = Direction.RIGHT;
+enemy_route[24][10] = Direction.DOWN;
+enemy_route[27][10] = Direction.RIGHT;
+enemy_route[27][13] = Direction.DOWN;
+enemy_route[30][13] = Direction.LEFT;
+enemy_route[30][2] = Direction.UP;
+enemy_route[27][2] = Direction.RIGHT;
+
+
+enemy_route[27][19] = Direction.UP;
+enemy_route[24][19] = Direction.RIGHT;
+enemy_route[24][22] = Direction.DOWN;
+enemy_route[27][22] = Direction.RIGHT;
+enemy_route[27][27] = Direction.DOWN;
+enemy_route[30][27] = Direction.LEFT;
+enemy_route[30][16] = Direction.UP;
+enemy_route[27][16] = Direction.RIGHT; 
+const $lifes  = document.querySelector(".lifes-container");
+ 
+export const drawLifes = (lifes) =>  {
+    let innerContent = "";
+    for (let i = 0; i < lifes; i++) {
+        innerContent += `<img class="heart" src="./assets/heart.png" alt="Lifes">`
+    }
+    $lifes.innerHTML = innerContent    
+}
 export const pacman = {
   name: "Pacman",
   img: "./assets/entities/pacman.png",
-  x: 2,
-  y: 2,
+  x: 14,
+  y: 18,
+  previousX: 14,
+  previousY: 18,
   speed: 1,
-  size: 30,
+  size: 25,
   moving_to: Direction.NO_MOVE,
   animation_right: "",
   animation_down: "",
   animation_up: "",
   animation_die: "",
+  coins: 0,
+  bfs: null,
+  lifes: 3,
+  enemies: [],
+  ghostTime: 7000,
+  resetEnemies: null,
+  original_map_design : null,
+  ghostMode: null,
+  nextLevel: null,
+  hasWon: false,
+
+  drawLifes: function (val = this.lifes) {
+    drawLifes(val)
+  },
+  drawCoinNumber: function (val = this.coins) {
+    document.getElementById("coin-ammount").innerHTML = val;
+  },
+  activateGhostMode() {
+    console.log("ghost mode started");
+    
+    this.enemies.map((enemy) => {
+      enemy.convertToGhost();
+    })
+  },
+  desactivateGhostMode() {
+    console.log("ghost mode ended ");
+    this.enemies.map((enemy) => {
+      enemy.revertFromGhost();
+    })
+  },
+  gameWined() {
+    console.log("game wined");
+    this.hasWon = true;
+    this.nextLevel();
+
+  },
+  eat_food: function (map_design) {
+    if (map_design[this.y][this.x] & 4) {
+      this.coins += 500;
+
+      map_design[this.y][this.x] = 1;
+      this.drawCoinNumber();
+      this.activateGhostMode();
+      clearTimeout(this.ghostMode);
+      this.ghostMode = setTimeout(() => {
+        this.desactivateGhostMode();
+      } , this.ghostTime)
+
+    }
+    if (map_design[this.y][this.x] & 2) {
+      this.coins+=100;
+      map_design[this.y][this.x] = 1;
+      this.drawCoinNumber();
+      this.hasWon = false
+    }
+
+
+    console.log( this.coins % 26000 == 0 );
+    
+    if (this.coins % 26000 == 0 && !this.hasWon) {
+      this.gameWined()
+    }
+  },
   could_move: function (direction, map_design) {
     /* Portals */
     if (direction == Direction.LEFT && this.x == 2 && this.y == 15) {
@@ -41,35 +147,498 @@ export const pacman = {
     }
     return false;
   },
-  move_pacman: function (map_design) {
+  
+  die(){
+    //What happens wen pacman dies
+    this.lifes--;
+    this.x = 14;
+    this.y = 18;
+    this.resetEnemies()
+    drawLifes(this.lifes)
+    
+    
+    if (this.lifes == 0) {
+      console.log(this.lifes);
+      showGameOver()
+    }
+  },
+  /**
+   * Mueve a pacman en la dirección especificada por this.moving_to
+   * Si pacman no puede moverse en esa dirección, no hace nada
+   * Si pacman puede moverse, actualiza su posición y come la comida
+   * que esté en esa posición
+   * Luego, calcula la ruta más corta a los fantasmas y la devuelve
+   * @param {Array<Array<number>>} map_design - El diseño del mapa
+   * @param {Array<Enemy>} enemies - Los fantasmas
+   * @returns {Array<Array<number>>} La ruta más corta a los fantasmas
+   */
+  move_pacman: function (map_design, enemies) {
+    let itCouldMove = false;
+
     /* PORTALS */
     if (this.moving_to == Direction.LEFT && this.x == 2 && this.y == 15) {
+      itCouldMove = true
+      this.previousX = this.x;
       this.x = 28;
     }
     if (this.moving_to == Direction.RIGHT && this.x == 27 && this.y == 15) {
+      itCouldMove = true
+      this.previousX = this.x;
       this.x = 1;
     }
-
+    /* PORTALS */
+    
     if (
       this.moving_to == Direction.LEFT &&
       map_design[this.y][this.x - 1] & 1
     ) {
+      itCouldMove = true
+      this.previousX = this.x;
       this.x -= this.speed;
     }
     if (
       this.moving_to == Direction.RIGHT &&
       map_design[this.y][this.x + 1] & 1
     ) {
+      itCouldMove = true
+      this.previousX = this.x;
       this.x += this.speed;
     }
     if (
       this.moving_to == Direction.DOWN &&
       map_design[this.y + 1][this.x] & 1
     ) {
+      itCouldMove = true
+      this.previousY = this.y;
       this.y += this.speed;
     }
     if (this.moving_to == Direction.UP && map_design[this.y - 1][this.x] & 1) {
+      itCouldMove = true
+      this.previousY = this.y;
       this.y -= this.speed;
     }
+
+
+    if (!itCouldMove) {
+      this.previousX = this.x;
+      this.previousY = this.y;
+    }
+    this.eat_food(map_design);
+
+    this.bfs = BFS(map_design, [this.y, this.x], enemies);
+
+    return this.bfs;
   },
 };
+
+export const minimumPaths = new Array();
+
+export class Enemy {
+  constructor(
+    color = "",
+    y = 24,
+    x = 14,
+    map_design = [],
+    enemy_route_main_point = {y: 6 , x: 7},
+    start = {y: 2 , x: 2},
+    delay = 0.2,
+    img = "",
+    speed = 1,
+    size = 25,
+    moving_to = Direction.RIGHT,
+    animation_right = "",
+    animation_down = "",
+    animation_up = "",
+    animation_die = "",
+    coins = 0
+  ) {
+    this.isInPrison = true;
+    this.isAGhost = false;
+    this.hasReached = false;
+    this.color = color;
+    this.img = img;
+    this.x = x;
+    this.y = y;
+    this.start = start;
+    this.previousX = x;
+    this.previousY = y;
+    this.speed = speed;
+    this.size = size;
+    this.map_design = map_design;
+    this.delay = delay;
+    this.moving_to = moving_to;
+    this.animation_right = animation_right;
+    (this.animation_down = animation_down), (this.animation_up = animation_up);
+    this.animation_die = animation_die;
+    this.coins = coins;
+    clearTimeout(this.appearTimeout);
+    this.appearTimeout =  setTimeout(() => {
+      this.x = 15
+      this.y = 12
+    }, delay * 1000);
+  }
+
+  reset(){
+    this.isInPrison = true;
+    this.isAGhost = false;
+    this.hasReached = false;
+    this.x = this.start.x;
+    this.y = this.start.y;
+    this.previousX = this.start.x;
+    this.previousY = this.start.y;
+    console.log("reseet");
+    
+    clearTimeout(this.appearTimeout);
+    this.appearTimeou= setTimeout(() => {
+      this.x = 15
+      this.y = 12
+     } , this.delay * 1000)
+  }
+  convertToGhost(){
+    this.isAGhost = true; 
+  }
+  revertFromGhost(){
+    this.isAGhost = false; 
+  }
+  freeFromPrision(){
+    this.isInPrison = false;
+    this.x= 14
+    this.y= 12 
+  }
+  sendToPrison(){
+    this.isInPrison = true;
+    this.x= this.start.x
+    this.y= this.start.y
+    this.previousX= this.start.x
+    this.previousY= this.start.y
+    this.freeFromPrision()
+  }
+  hasTouchedPacman(){
+    let res = false;
+   
+    if((this.x == pacman.x && this.y== pacman.y) || (this.previousX == pacman.x && this.previousY == pacman.y && pacman.previousX   )){
+      res = true;
+      console.log("isAGhost" , this.isAGhost);
+      
+    }
+    else res = false
+    
+    if(this.isAGhost && ((this.x == pacman.x && this.y== pacman.y) || (this.previousX == pacman.x && this.previousY == pacman.y ))){
+      this.sendToPrison()
+    }
+    if(!this.isAGhost && ((this.x == pacman.x && this.y== pacman.y) || (this.previousX == pacman.x && this.previousY == pacman.y ))){
+      pacman.die()
+
+    }
+    return res;
+      
+  }
+
+  couldMove(direction, map_design) {
+    /* Portals */
+    if (direction == Direction.LEFT && this.x == 2 && this.y == 15) {
+      return true;
+    }
+    if (direction == Direction.RIGHT && this.x == 27 && this.y == 15) {
+      return true;
+    }
+    /* Portals */
+    if (direction == Direction.LEFT && map_design[this.y][this.x - 1] & 1) {
+      return true;
+    }
+    if (direction == Direction.RIGHT && map_design[this.y][this.x + 1] & 1) {
+      return true;
+    }
+    if (direction == Direction.DOWN && map_design[this.y + 1][this.x] & 1) {
+      return true;
+    }
+    if (direction == Direction.UP && map_design[this.y - 1][this.x] & 1) {
+      return true;
+    }
+    return false;
+  }
+  changeDirection(map_design) {
+    const available_directions = [];
+
+    if (this.couldMove(Direction.LEFT, map_design)) {
+      available_directions.push(Direction.LEFT);
+    }
+    if (this.couldMove(Direction.RIGHT, map_design)) {
+      available_directions.push(Direction.RIGHT);
+    }
+    if (this.couldMove(Direction.DOWN, map_design)) {
+      available_directions.push(Direction.DOWN);
+    }
+    if (this.couldMove(Direction.UP, map_design)) {
+      available_directions.push(Direction.UP);
+    }
+
+    this.moving_to =
+      available_directions[
+        Math.floor(Math.random() * available_directions.length)
+      ];
+  }
+
+  /**
+   * 
+   * @param {<Array>Array} bfsArray the result of call BFS function
+   * @returns A boolean, true for indicate the enemy has reached the target, false for the oposite
+   */
+  moveByBFS(bfsArray , moveHandler = true) {
+    if (!moveHandler){
+    this.hasTouchedPacman();
+  return;
+    }
+    const directions = [
+      { dx: 0, dy: -1 }, // Arriba
+      { dx: 0, dy: 1 }, // Abajo
+      { dx: -1, dy: 0 }, // Izquierda
+      { dx: 1, dy: 0 }, // Derecha
+    ];
+
+    let minSteps = Infinity;
+    let candidates = [];
+    let available_directions = [];
+
+    // Revisar las cuatro direcciones posibles
+    for (let dir of directions) {
+      let newX = this.x + dir.dx;
+      let newY = this.y + dir.dy;
+      if(newX == this.previousX && newY == this.previousY) continue;
+
+      // Asegúrate de que la nueva posición esté dentro de los límites del arreglo
+      if (newX == 1 && newY == 15) {
+        newX = 27;
+        newY = 15;
+      }
+      if (newX == 28 && newY == 15) {
+        newX = 2;
+        newY = 15;
+      }
+      
+      if (bfsArray[newY][newX]) {
+        const steps = bfsArray[newY][newX];
+
+          // Si encontramos un nuevo mínimo, reiniciamos los candidatos
+          if (steps < minSteps) {
+            minSteps = steps;
+            candidates = [{ x: newX, y: newY }];
+          } else if (steps === minSteps) {
+            // Si es igual al mínimo, lo añadimos a los candidatos
+            candidates.push({ x: newX, y: newY });
+        } 
+      }
+      if (this.map_design[newY][newX] & 1) {
+        available_directions.push(dir);
+      }
+          }
+    // Elegir un candidato al azar si hay múltiples
+
+    if (candidates.length > 0) {
+      const chosen = candidates[Math.floor(Math.random() * candidates.length)];
+      this.previousX = this.x;
+      this.previousY = this.y;
+      this.x = chosen.x;
+      this.y = chosen.y;
+    }
+    this.hasTouchedPacman()
+
+  }
+
+  moveEnemy( map_design, bfsArray , moveHandler) {
+
+  
+
+    if (!this.hasReached) {
+      this.hasReached = this.moveByBFS(bfsArray , moveHandler);
+    }else  {
+
+      if (!moveHandler){
+        this.hasTouchedPacman();
+      return;
+        }
+      this.moveInRoute(map_design)
+    }
+  }
+
+
+  moveInRoute( ) {
+    if (enemy_route[this.y][this.x]) {
+      this.moving_to = enemy_route[this.y][this.x]
+    }
+    
+    if (
+      this.moving_to == Direction.LEFT 
+    ) {
+      this.previousX = this.x;
+      this.x -= this.speed;
+    }
+    if (
+      this.moving_to == Direction.RIGHT 
+    ) {
+      this.previousX = this.x;
+      this.x += this.speed;
+    }
+    if (
+      this.moving_to == Direction.DOWN 
+    ) {
+      this.previousX = this.y;
+      this.y += this.speed;
+    }
+    if (this.moving_to == Direction.UP ) {
+      this.previousX = this.y;
+      this.y -= this.speed;
+    }
+
+
+
+  }
+  moveToOpositeDirection() {
+    this.previousX  = this.x
+    this.previousY  = this.y
+    if (this.moving_to == Direction.LEFT) {
+      this.moving_to = Direction.RIGHT;
+    }
+    if (this.moving_to == Direction.RIGHT) {
+      this.moving_to = Direction.LEFT;
+    }
+    if (this.moving_to == Direction.DOWN) {
+      this.moving_to = Direction.UP;
+    }
+    if (this.moving_to == Direction.UP) {
+      this.moving_to = Direction.DOWN;
+    }
+  }
+  moveAsGhost(bfsArray , moveHandler = true) {
+    if (!moveHandler) {
+        this.hasTouchedPacman();
+        return;
+    }
+    const directions = [
+        { dx: 0, dy: -1 }, // Arriba
+        { dx: 0, dy: 1 },  // Abajo
+        { dx: -1, dy: 0 }, // Izquierda
+        { dx: 1, dy: 0 },  // Derecha
+    ];
+
+    let maxSteps = -Infinity;
+    let candidates = [];
+    let available_directions = [];
+
+    // Revisar las cuatro direcciones posibles
+    for (let dir of directions) {
+        let newX = this.x + dir.dx;
+        let newY = this.y + dir.dy;
+        if (newX === this.previousX && newY === this.previousY) continue;
+
+        // Verificar los límites de las posiciones
+        if (newX === 1 && newY === 15) {
+            newX = 27;
+            newY = 15;
+        }
+        if (newX === 28 && newY === 15) {
+            newX = 2;
+            newY = 15;
+        }
+
+        // Revisar si la nueva posición está en el array y tiene un valor
+        if (bfsArray[newY][newX]) {
+            const steps = bfsArray[newY][newX];
+
+            // Si encontramos un nuevo máximo, reiniciamos los candidatos
+            if (steps > maxSteps) {
+                maxSteps = steps;
+                candidates = [{ x: newX, y: newY }];
+            } else if (steps === maxSteps) {
+                // Si es igual al máximo, lo añadimos a los candidatos
+                candidates.push({ x: newX, y: newY });
+            } 
+        }
+        if (this.map_design[newY][newX] & 1) {
+            available_directions.push(dir);
+        }
+    }
+
+    // Elegir un candidato al azar si hay múltiples
+    if (candidates.length > 0) {
+        const chosen = candidates[Math.floor(Math.random() * candidates.length)];
+        this.previousX = this.x;
+        this.previousY = this.y;
+        this.x = chosen.x;
+        this.y = chosen.y;
+    }
+    this.hasTouchedPacman();
+}
+
+}
+
+
+
+/**
+ * 
+ * @param {<Array>Array} matrix The map design 
+ * @param {Array} start The y and x in [y,x] format
+ * @returns A matrix with the cost of access to every point from the y,x coordinates. 
+ */
+
+export function BFS(matrix, start) {
+  let bfsArray = Array.from({ length: matrix.length }, () =>
+    new Array(matrix[1].length).fill(null)
+  );
+
+  let visited = new Set([start.toString()]);
+  let directions = [
+    [0, 1], // right
+    [1, 0], // down
+    [0, -1], // left
+    [-1, 0], // up
+  ];
+
+  let [currentY, currentX] = start;
+  let queue = [[currentY, currentX, 1]]; // Each entry: [y, x, level]
+
+  while (queue.length > 0) {
+    let [currentY, currentX, level] = queue.shift();
+
+    bfsArray[currentY][currentX] = level;
+
+    for (let [dy, dx] of directions) {
+      let newY = currentY + dy;
+      let newX = currentX + dx;
+      let newLevel = level + 1;
+
+      // Boundary and obstacle check
+      if (
+        newY >= 1 &&
+        newY < matrix.length &&
+        newX >= 1 &&
+        newX < matrix[1].length &&
+        matrix[newY][newX] & 1 && // Adjust for obstacles (if any)
+        !visited.has([newY, newX].toString())
+      ) {
+        visited.add([newY, newX].toString());
+        queue.push([newY, newX, newLevel]);
+      }
+
+      /* PORTALS */
+      if (newY == 15 && newX == 28 && !visited.has([15, 2].toString())) {
+        queue.push([15, 2, newLevel]);
+        visited.add([15, 2].toString());
+      }
+      if (newY == 15 && newX == 1 && !visited.has([15, 27].toString())) {
+        queue.push([15, 27, newLevel]);
+        visited.add([15, 27].toString());
+      }
+      /* PORTALS */
+    }
+  }
+
+  return bfsArray;
+}
+
+const showGameOver = () =>{
+  console.log("game over");
+  
+    const $gameOver = document.querySelector('.game-over');
+    $gameOver.style.opacity = 1;
+}
--- public/pacman.js
+++ public/pacman.js
@@ -1,30 +1,42 @@
-import { Direction, pacman } from "./entities.js";
+import { Direction, pacman, Enemy, minimumPaths, BFS , enemy_route , drawLifes } from "./entities.js";
+
 import { fillTheLevel } from "./fillTheLevel.js";
 
-console.log("pacman:", pacman);
 const canvas = document.getElementById("screen");
 /** @type {CanvasRenderingContext2D} */
 var ctx = canvas.getContext("2d");
-let map_design = new Array(1000);
 
 export const x = 25;
 const widthBlocks = 29;
 const heightBlocks = 32;
+let map_design = new Array(heightBlocks);
+drawLifes(3)
+
 
 const width = widthBlocks * x;
 const height = heightBlocks * x;
 canvas.setAttribute("width", `${width}`);
 canvas.setAttribute("height", `${height}`);
 
+const States = {
+  chase: 1,
+  scatter: 2,
+  frightened: 3
+}
+
+let state = States.chase;
+
+
+
 for (let i = 1; i <= heightBlocks; i++) {
   map_design[i] = new Array(widthBlocks + 1);
   for (let j = 1; j <= widthBlocks; j++) {
-    map_design[i][j] = false;
+    map_design[i][j] = 0;
   }
 }
 // Declara el objeto 'keys' para almacenar el estado de las teclas
 const keys = {};
-
+const original_map_design = map_design;
 // Eventos para detectar cuando una tecla se presiona
 window.addEventListener("keydown", (e) => {
   keys[e.key] = true; // Marca la tecla como presionada
@@ -35,8 +47,72 @@ window.addEventListener("keyup", (e) => {
   keys[e.key] = false; // Marca la tecla como liberada
 });
 
+
+// Variables para detectar el inicio y final del toque
+let startX, startY;
+
+// Evento para detectar cuando el usuario toca la pantalla
+window.addEventListener("touchstart", (e) => {
+  const touch = e.touches[0];
+  startX = touch.clientX;
+  startY = touch.clientY;
+});
+
+// Evento para detectar cuando el usuario mueve el dedo
+window.addEventListener("touchmove", (e) => {
+  e.preventDefault(); // Evitar el comportamiento predeterminado de desplazamiento
+});
+
+// Evento para detectar cuando el usuario levanta el dedo (fin del swipe)
+window.addEventListener("touchend", (e) => {
+  const touch = e.changedTouches[0];
+  const endX = touch.clientX;
+  const endY = touch.clientY;
+
+  const diffX = endX - startX;
+  const diffY = endY - startY;
+
+  // Determinar la dirección del deslizamiento
+  if (Math.abs(diffX) > Math.abs(diffY)) {
+    // Deslizamiento horizontal
+    if (diffX > 0) {
+      keys["ArrowRight"] = true; // Marca la flecha derecha como presionada
+      setTimeout(() => (keys["ArrowRight"] = false), 100); // Libera la tecla después de un momento
+    } else {
+      keys["ArrowLeft"] = true; // Marca la flecha izquierda como presionada
+      setTimeout(() => (keys["ArrowLeft"] = false), 100);
+    }
+  } else {
+    // Deslizamiento vertical
+    if (diffY > 0) {
+      keys["ArrowDown"] = true; // Marca la flecha abajo como presionada
+      setTimeout(() => (keys["ArrowDown"] = false), 100);
+    } else {
+      keys["ArrowUp"] = true; // Marca la flecha arriba como presionada
+      setTimeout(() => (keys["ArrowUp"] = false), 100);
+    }
+  }
+});
+
 fillTheLevel(map_design);
 
+const violetRouteBFS = BFS(map_design,[6,7])
+const redRouteBFS = BFS(map_design,[6,22])
+const orangeRouteBFS = BFS(map_design,[24,10])
+const yellowRouteBFS = BFS(map_design,[24,22])
+export let enemies = [
+  new Enemy("red", 14, 16, map_design, {y:6 , x:22}  , {y:14 , x:16}  ,  1),
+  new Enemy("violet", 14, 13, map_design, {y:6 , x:7}, {y:14 , x:13}  ,6),
+  new Enemy("orange", 16, 13, map_design,{y:24 , x:10} ,{y:16 , x:13} , 11),
+  new Enemy("green", 16, 16, map_design, {y:24 , x:22},{y:16 , x:16}  ,21),
+];
+
+pacman.nextLevel = ()=>{
+  reset(true)
+}
+
+pacman.enemies= enemies;
+pacman.resetEnemies= resetEnemies;
 const img = new Image(); // Create a new image object
 img.src = "./assets/map.webp";
 img.onload = () => {
@@ -50,18 +126,39 @@ img.onload = () => {
     }
   }
 };
-
+const renderInterval = 0.2;
+const changeStateInterval = 10;
 let frame = 0;
-// Bucle principal del juego
-function gameLoop() {
+let accumulatedTime = 0;
+let accumulatedTime10 = 0;
+let lastTime = 0;
+function gameLoop(currentTime) {
   frame++;
-  // Actualiza la lógica del juego
+  let deltaTime = (currentTime - lastTime) / 1000;
+  lastTime = currentTime;
+  accumulatedTime += deltaTime;
+  accumulatedTime10 +=deltaTime;
+  // Check if 0.3 seconds have passed
+  if (accumulatedTime >= renderInterval) {
+    render();
+    accumulatedTime = 0; // Reset the accumulated time
+  }
+  if (accumulatedTime10 >= changeStateInterval) {
+    console.log("change status");
+    
+    if (state == States.chase) {
+      state = States.scatter
+      enemies[0].hasReached = false
+      enemies[1].hasReached = false
+      enemies[2].hasReached = false
+      enemies[3].hasReached = false
+    }else if (state == States.scatter) {
+      state = States.chase
+    }
+    accumulatedTime10 = 0;
+  }
 
   update();
-  // Dibuja el estado del juego
-  if (frame % 30 == 0) {
-    render();
-  }
 
   requestAnimationFrame(gameLoop);
   // Llama a gameLoop en el siguiente fotograma
@@ -85,27 +182,162 @@ function update() {
   }
 }
 
-// Dibuja el estado del juego en el canvas
+
+function drawCircle(canvasContext, x, y, shapeHeight) {
+  const halfShapeHeight = shapeHeight / 2;
+  canvasContext.beginPath();
+  canvasContext.arc(x, y, halfShapeHeight, 0, 2 * Math.PI);
+  canvasContext.stroke();
+}
+
+let renderCount = 0;
 function render() {
-  pacman.move_pacman(map_design);
+  renderCount++;
+  let moveHandler = true;
+  const pacmanBFS = pacman.move_pacman(map_design, enemies);
+  const routeBFSArrays = [redRouteBFS, violetRouteBFS, orangeRouteBFS, yellowRouteBFS]
+  enemies.map((enemy , i) => {
+    moveHandler =renderCount%2 ==0
+
+    switch (state) {
+      case States.scatter:
+        if (enemy.isAGhost) {
+
+          enemy.moveToOpositeDirection()
+          enemy.moveAsGhost( pacmanBFS,  moveHandler)
+        }
+        else
+        enemy.moveEnemy( map_design, routeBFSArrays[i])
+      break;
+      case States.chase:
+        if (enemy.isAGhost) {
+          enemy.moveToOpositeDirection()
+          enemy.moveAsGhost( pacmanBFS,  moveHandler)
+        }
+          else  
+        enemy.moveByBFS(pacmanBFS ) ;
+        break;
+      
+    } 
+  })
+  
+  
+/*   enemies[1].moveToPacman(pacmanBFS);
+  enemies[2].moveToPacman(pacmanBFS);
+  enemies[3].moveToPacman(pacmanBFS); */
   // Limpia el canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   ctx.drawImage(img, x, x, width - 20, height - 25);
   for (let i = 1; i <= heightBlocks; i++) {
     for (let j = 1; j <= widthBlocks; j++) {
-      if (map_design[i][j] & 1) {
-        ctx.fillStyle = "white";
-        ctx.fillRect(j * x, i * x, x, x);
+      if (map_design[i][j] & 2) {
+        ctx.fillStyle = "yellow";
+        drawCircle(ctx, j * x + 10, i * x + 10, 5);
+        ctx.fill();
       }
     }
   }
-  // ctx.drawImage(img, x, x, width - 20, height - 25);
-  console.log("x , y", pacman.x, pacman.y);
+  
+  for (let i = 1; i <= heightBlocks; i++) {
+    for (let j = 1; j <= widthBlocks; j++) {
+      if (map_design[i][j] & 4) {
+        drawCircle(ctx, j * x + 10,i * x + 10, 18);
+        ctx.fill()
+        
+      }
+    }
+  }
 
-  // Dibuja al jugador
-  ctx.fillStyle = "blue";
+  // ctx.drawImage(img, x, x, width - 20, height - 25);
+  // Painting Pacman
+  ctx.fillStyle = "yellow";
   ctx.fillRect(pacman.x * x, pacman.y * x, pacman.size, pacman.size);
+
+  // Painting Enemies
+
+  for (let i = 0; i < enemies.length; i++) {
+    const enemy = enemies[i];
+
+    ctx.fillStyle = enemy.color;
+    ctx.fillRect(enemy.x * x, enemy.y * x, enemy.size, enemy.size);
+  }
+/* 
+  for (let i = 1; i <= heightBlocks; i++) {
+    for (let j = 1; j <= widthBlocks; j++) {
+      if (map_design[i][j] & 1) {
+        ctx.fillStyle = "red";
+        ctx.fillText(enemy_route[i][j], j * x + 8, i * x + 8);
+      }
+    }
+  } */
+  /* for (let i = 1; i <= heightBlocks; i++) {
+    for (let j = 1; j <= widthBlocks; j++) {
+      if (map_design[i][j] & 1) {
+        ctx.fillStyle = "red";
+        ctx.fillText(pacman.bfs[i][j], j * x + 8, i * x + 8);
+      }
+    }
+  } */
 }
 
-gameLoop();
+/**
+ * Resets Pacman and the enemies to their initial state
+ * @return {undefined}
+ */
+const reset = (nextLevel= false) => {
+  pacman.x = 14;
+  pacman.y = 18;
+  pacman.previousX = 14;
+  pacman.previousY = 18;
+  pacman.speed = 1;
+  pacman.size = 25;
+  pacman.moving_to = Direction.NO_MOVE;
+  if (!nextLevel) {
+    pacman.lifes = 3;
+    pacman.coins = 0;
+    
+  }
+  pacman.drawCoinNumber(pacman.coins);
+  pacman.drawLifes()
+  clearTimeout(pacman.ghostMode);
+
+  enemies =  [ new Enemy("red", 14, 16, map_design, {y:6 , x:22}  , {y:14 , x:16}  ,  1),
+  new Enemy("violet", 14, 13, map_design, {y:6 , x:7}, {y:14 , x:13}  ,6),
+  new Enemy("orange", 16, 13, map_design,{y:24 , x:10} ,{y:16 , x:13} , 11),
+  new Enemy("green", 16, 16, map_design, {y:24 , x:22},{y:16 , x:16}  ,21),
+]
+fillTheLevel(map_design);
+pacman.enemies = enemies;
+   
+};
+
+function resetEnemies(){
+   clearTimeout(pacman.ghostMode);
+  enemies =  [ new Enemy("red", 14, 16, map_design, {y:6 , x:22}  , {y:14 , x:16}  ,  1),
+    new Enemy("violet", 14, 13, map_design, {y:6 , x:7}, {y:14 , x:13}  ,6),
+    new Enemy("orange", 16, 13, map_design,{y:24 , x:10} ,{y:16 , x:13} , 11),
+    new Enemy("green", 16, 16, map_design, {y:24 , x:22},{y:16 , x:16}  ,21),
+  ] 
+  pacman.enemies = enemies;
+};
+
+
+document.addEventListener("keydown", (event) => {  
+  if (event.key === " " && pacman.lifes <= 0) {
+    const $gameOver = document.querySelector(".game-over");
+    $gameOver.style.opacity = "0";
+    reset();
+  }
+});
+
+document.addEventListener("touchend", (event) => {  
+  if ( pacman.lifes <= 0) {
+    const $gameOver = document.querySelector(".game-over");
+    $gameOver.style.opacity = "0";
+    reset();
+  }
+});
+gameLoop(0);
+
+
