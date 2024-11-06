b473c71dafead236c684ae19f80cfa0d4941e0bf
freddysae0 <freddysae00@gmail.com>
2024-11-06 12:28:53 +0100

    node config and gitignore

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const express = require("express");
 const path = require("path");
 const livereload = require("livereload");
 const connectLivereload = require("connect-livereload");
-
+/* 
+const { fillTheLevel } = require("./public/fillTheLevel.js"); // Añadir
+const { BFS } = require("./public/entities.js"); // Añadir */
 const app = express();
 const port = 3000;
 
@@ -20,7 +22,26 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
+/* 
+app.get("/test", (req, res) => {
+  const x = 25;
+  const widthBlocks = 29;
+  const heightBlocks = 32;
+  let map_design = new Array(heightBlocks);
 
+  for (let i = 1; i <= heightBlocks; i++) {
+    map_design[i] = new Array(widthBlocks + 1);
+    for (let j = 1; j <= widthBlocks; j++) {
+      map_design[i][j] = 0;
+    }
+  }
+
+  const level = fillTheLevel(map_design);
+  const bfsArray = BFS(level, [2, 2]);
+
+  res.send(bfsArray);
+});
+ */
 // Iniciar el servidor
 app.listen(port, () => {
   console.log(`Server running en http://localhost:${port}`);
